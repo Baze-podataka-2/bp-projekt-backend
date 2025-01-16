d78fe3863e047f0cfadb8c7fe5d6a5f69f9dce42
spahicadev <adis@spahica.com>
2025-01-16 02:18:26 +0100

    Added get route for single equipment

--- routes/equipmentRouter.js
+++ routes/equipmentRouter.js
@@ -12,6 +12,24 @@ equipmentRouter.get('/', (req, res) => {
     })
 })
 
+// Get single equipment
+equipmentRouter.get('/:id', async(req, res) => {
+    const { id } = req.params;
+    if(!id) {
+        return res.status(404).send({message: 'Oprema nije pronadena' });
+    }
+
+    let sql = 'SELECT * FROM oprema WHERE id = ?'
+
+    db.query(sql, [id], (err, results) => {
+        if(err) {
+            throw err;
+        }
+        res.json(results);
+    })
+
+})
+
 //jos get single equipment
 //delete, update razmisliti na ovoj ruti jer se vecina toga dogada preko procedura u bazi, ali moze npr biti azuriraj stanje na zalihama opreme za sada
 
