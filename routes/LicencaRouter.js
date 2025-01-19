1527f7a43255421e43ae660cbb8415d5fc75a30f
Ronan <ronan.krebel@gmail.com>
2025-01-19 22:44:12 +0100

    Fixed route for licence

--- routes/LicencaRouter.js
+++ routes/LicencaRouter.js
@@ -6,7 +6,9 @@ const LiRouter = new express.Router();
 
 LiRouter.get("/aktivnost/:id", async(req, res)=>{
     const { id } = req.params;
-    let sql = 'CALL AktivnostLicence(?, @status);';
+
+    let sql = 'CALL AktivnostLicence(2, @poruka); SELECT @poruka FROM DUAL';
+
 
     db.query(sql, [id], (err, results) => {
         res.json(results);
