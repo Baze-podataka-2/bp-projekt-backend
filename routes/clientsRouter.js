fef8cdf8c8af439fb579bbbc5d522ffb05fc36ae
spahicadev <adis@spahica.com>
2025-01-19 01:14:01 +0100

    Change service for client works well

--- routes/clientsRouter.js
+++ routes/clientsRouter.js
@@ -12,16 +12,32 @@ clientRouter.get('/', (req, res) => {
     })
 })
 
-// Route that will calls a procedure
-clientRouter.get('/', (req, res) => {
-    const {id_klijent, id_usluga} = req.query;
+clientRouter.get('/azuriraj', (req, res) => {
+    const { id_klijent, id_usluga } = req.query;
 
-    let sql = 'call PromjenaUslugeKlijenta(? ?)';
+    if (!id_klijent || !id_usluga) {
+        return res.status(400).json({ error: 'id_klijent i id_usluga su obavezni parametri.' });
+    }
 
-    db.query(sql, (err, results) => {
-        return res.status(200).json(results);
-    })
-})
+    // SQL query s procedurom i izlaznim parametrom
+    let sql = `
+        CALL PromjenaUslugeKlijenta(?, ?, @poruka);
+        SELECT @poruka AS poruka;
+    `;
+
+    db.query(sql, [id_klijent, id_usluga], (err, results) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Dogodila se greška pri izvršavanju procedure.' });
+        }
+
+        // Povlačenje izlaznog parametra @poruka
+        const message = results[1][0].poruka;
+
+        return res.status(200).json({ message });
+    });
+});
+
 
 
 export default clientRouter;
